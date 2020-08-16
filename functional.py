c9f16be8740a5729b52844fe6c2d976e925f1930
paletteOvO <paletteOvO@outlook.com>
2020-08-16 12:37:46 +0800

    add take

--- functional.py
+++ functional.py
@@ -6,3 +6,12 @@ def flatten(x):
       else:
          a.append(i)
    return a
+
+def take(n, it):
+   i = 0
+   for k in it:
+      if i >= n:
+         return
+      yield k
+      i += 1
+   return
